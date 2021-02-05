36cfabb3140323e3d25b7a5cb9d807aad0201f50
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2021-02-05 13:50:27 +0100

    first commit

---
+++ src/AcdhchExt/SolrMarc.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AcdhchExt;
+
+class SolrMarc extends \AkSearch\RecordDriver\SolrMarc {
+    public function getThumbnail($size = 'small'){
+        return 'https://fundament.acdh.oeaw.ac.at/common-assets/images/acdh_logo.svg';
+    }
+}
